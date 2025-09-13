0daed4df45fcebbddabc325f6004a411e6f2f7c8
Dmitry Nasonov <1260248+gvard@users.noreply.github.com>
2025-09-13 16:57:42 +0300

    Added code initially written in August 2023 for E&S Digistar interface
    This simple python app has been running since August 2023 in the Moscow Planetarium, Big Star Hall

---
+++ cal.py
@@ -0,0 +1,48 @@
+"""Code for making requests to the Google Calendar API and working with Google Calendar events
+See Python quickstart guide:
+https://developers.google.com/workspace/calendar/api/quickstart/python
+Google Calendar API reference:
+https://developers.google.com/workspace/calendar/api/v3/reference
+"""
+
+
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+
+
+TIMEZONE = "Europe/Moscow"
+TZ_DELTA = 3  # Time difference in hours for Europe/Moscow timezone
+
+
+class GoogleCalendar:
+    SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+    FILE_PATH = "your_key_filename.json"
+
+    def __init__(self):
+        creds = service_account.Credentials.from_service_account_file(
+            filename=self.FILE_PATH, scopes=self.SCOPES
+        )
+        self.service = build("calendar", "v3", credentials=creds)
+
+    def get_calendar_list(self):
+        return self.service.calendarList().list().execute()
+
+    def add_calendar(self, calendar_id):
+        calendar_list_entry = {"id": calendar_id}
+        return self.service.calendarList().insert(body=calendar_list_entry).execute()
+
+    def list_event(self, calendar_id, start, end):
+        return (self.service.events().list(
+                calendarId=calendar_id,
+                timeMin=start,
+                timeMax=end,
+                singleEvents=True,
+                orderBy="startTime",
+                timeZone=TIMEZONE,
+                ).execute())
+
+
+CAL_ID = "yourcalendaridhere.calendar.google.com"
+
+# Add calendar once
+# Gcal.add_calendar(calendar_id=CAL_ID)
---
+++ flask_cal.py
@@ -0,0 +1,111 @@
+"""Daily schedule data from Google Calendar taken by the list_event method and visualized using Flask
+"""
+
+import os
+import ssl
+from datetime import datetime, timedelta
+from time import ctime
+import locale
+import json
+import urllib.request
+
+from flask import Flask, render_template
+
+from cal import GoogleCalendar, CAL_ID, TZ_DELTA
+
+
+PRINT_MSG = False
+HOST = "127.0.0.1"
+PORT = 5000
+NOEVENT_MSG = "10:00–20:00", "Сегодня без событий!"
+
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+    getattr(ssl, '_create_unverified_context', None)):
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def get_yandex_time():
+    """Get timestamp from time.yandex.ru for Moscow, return datetime object
+    """
+    locale.setlocale(locale.LC_ALL, "C")
+    URL = "https://yandex.com/time/sync.json?geo=213"
+    time_data_json = json.load(urllib.request.urlopen(URL))
+    yandex_time = time_data_json.get("time")
+    return yandex_time
+
+
+def get_time(time_now, dlt=3):
+    start = (datetime(time_now.year, time_now.month, time_now.day,
+                      9-dlt, 59)).isoformat() + "Z"
+    end = (datetime(time_now.year, time_now.month, time_now.day,
+                    21-dlt, 1)).isoformat() + "Z"
+    return start, end
+
+
+def get_times(strt, end):
+    strt = strt.split("T")[1][:5]
+    end = end.split("T")[1][:5]
+    return "–".join((strt, end))
+
+
+def mk_req(start, end):
+    events = Gcal.list_event(CAL_ID, start, end)
+    return events
+
+
+beautify = lambda st: st.strip(".").replace("солнечн", "Солнечн").replace("вселенн", "Вселенн")
+
+Gcal = GoogleCalendar()
+app = Flask(__name__)
+
+
+@app.route("/")
+def render_cal():
+    yandex_ctime = get_yandex_time()
+    time_now = datetime.strptime(ctime(yandex_ctime / 1000), "%c")
+    gdate = time_now.date()
+    start, end = get_time(time_now, dlt=TZ_DELTA)
+    events = mk_req(start, end)
+    locale.setlocale(locale.LC_ALL, "ru_RU")
+    today_dayweek = datetime.now().strftime("%A")
+    data = []
+    for evnt in events["items"]:
+        if today_dayweek == "вторник" and datetime.now().month != 1:
+            data = [(NOEVENT_MSG)]
+        else:
+            time_str = get_times(evnt["start"]["dateTime"], evnt["end"]["dateTime"])
+            if PRINT_MSG:
+                print(time_str, evnt["summary"])
+            data.append((time_str, beautify(evnt["summary"])))
+    return render_template("daily_with_hidden.html", data=data, today=today_dayweek, tim=time_now.time(), gdat=gdate)
+
+
+@app.route("/tomorrow/")
+def render_cal_tomorrow():
+    # Yandex ctime increased by one day
+    yandex_ctime = get_yandex_time() + 86400000
+    time_now = datetime.strptime(ctime(yandex_ctime / 1000), "%c")
+    gdate = time_now.date()
+    start, end = get_time(time_now, dlt=TZ_DELTA)
+    events = mk_req(start, end)
+    locale.setlocale(locale.LC_ALL, "ru_RU")
+    tomorrow = datetime.now() + timedelta(days=1)
+    today_dayweek = tomorrow.strftime("%A")
+    data = []
+    for evnt in events["items"]:
+        if today_dayweek == "вторник" and tomorrow.month != 1:
+            data = [(NOEVENT_MSG)]
+        else:
+            time_str = get_times(evnt["start"]["dateTime"], evnt["end"]["dateTime"])
+            if PRINT_MSG:
+                print(time_str, evnt["summary"])
+            data.append((time_str, beautify(evnt["summary"])))
+    return render_template("daily_with_hidden.html", data=data, today=today_dayweek, tim=time_now.time(), gdat=gdate)
+
+
+@app.route("/hidden/")
+def render_hidden():
+    return render_template("hidden.html")
+
+if __name__ == "__main__":
+    app.run(host=HOST, port=PORT)
