b1d78691d9e0f2232dea1e3d269ab165fdc710cf
Dmitry Nasonov <1260248+gvard@users.noreply.github.com>
2025-09-20 21:30:11 +0300

    flask_cal now supports simultaneous launch of a telegram bot using threading

--- flask_cal.py
+++ flask_cal.py
@@ -8,27 +8,33 @@ from time import ctime
 import locale
 import json
 import urllib.request
+import threading
 
 from flask import Flask, render_template
+from telebot import TeleBot, types
 
 from cal import GoogleCalendar, CAL_ID, TZ_DELTA
 
 
+app = Flask(__name__)
+
+TG_BOT = True
+TG_TOKEN = "YOUR_TOKEN_HERE"
 PRINT_MSG = False
 HOST = "127.0.0.1"
 PORT = 5000
-NOEVENT_MSG = "10:00–20:00", "Сегодня без событий!"
+NOEVENT_MSG = "10:00–20:00", "No events today"
 
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
     getattr(ssl, '_create_unverified_context', None)):
     ssl._create_default_https_context = ssl._create_unverified_context
 
 
-def get_yandex_time():
+def get_yandex_time(geo=213):
     """Get timestamp from time.yandex.ru for Moscow, return datetime object
     """
     locale.setlocale(locale.LC_ALL, "C")
-    URL = "https://yandex.com/time/sync.json?geo=213"
+    URL = f"https://yandex.com/time/sync.json?geo={geo}"
     time_data_json = json.load(urllib.request.urlopen(URL))
     yandex_time = time_data_json.get("time")
     return yandex_time
@@ -55,8 +61,65 @@ def mk_req(start, end):
 
 beautify = lambda st: st.strip(".").replace("солнечн", "Солнечн").replace("вселенн", "Вселенн")
 
-Gcal = GoogleCalendar()
-app = Flask(__name__)
+
+def get_cal_evnts():
+    yandex_ctime = get_yandex_time()
+    time_now = datetime.strptime(ctime(yandex_ctime / 1000), "%c")
+    gdate = time_now.date()
+    start, end = get_time(time_now, dlt=TZ_DELTA)
+    events = mk_req(start, end)
+    locale.setlocale(locale.LC_ALL, "ru_RU")
+    today_dayweek = datetime.now().strftime("%A")
+    data = []
+    for evnt in events["items"]:
+        if today_dayweek == "вторник" and datetime.now().month != 1:
+            data = [(NOEVENT_MSG)]
+        else:
+            time_str = get_times(evnt["start"]["dateTime"], evnt["end"]["dateTime"])
+            print(time_str, evnt["summary"])
+            data.append((time_str, beautify(evnt["summary"])))
+    return data, time_now.time()
+
+
+def split_evnts(evnts):
+    evnts_str = ""
+    for evnt in evnts:
+        evnts_str += f"{evnt[0]} {evnt[1]}\n"
+    return evnts_str
+
+
+def telegram_bot():
+    bot = TeleBot(TG_TOKEN)
+
+    @bot.message_handler(commands=["start"])
+    def start_message(message):
+        MSG = "\n".join(("Hello and welcome to the daily scheduler bot.",
+                         "Type <i>today</i> to display the daily schedule.",
+                         ))
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        today = types.KeyboardButton("Today")
+        markup.row(today)
+        bot.send_message(
+            message.chat.id,
+            MSG,
+            reply_markup=markup,
+            parse_mode='HTML',
+            )
+
+    @bot.message_handler()
+    def send_text(message):
+        if message.text.lower() == "today":
+            events_arr, time_now = get_cal_evnts()
+            events_str = split_evnts(events_arr)
+            bot.send_message(
+                message.chat.id,
+                f"<u>Current time</u>: {time_now}\n\n{events_str}",
+                parse_mode='HTML',
+            )
+        else:
+            bot.send_message(message.chat.id, "Unknown command")
+
+    bot.polling(none_stop=True)
 
 
 @app.route("/")
@@ -108,4 +171,8 @@ def render_hidden():
     return render_template("hidden.html")
 
 if __name__ == "__main__":
+    Gcal = GoogleCalendar()
+    if TG_BOT:
+        bot_thread = threading.Thread(target=telegram_bot)
+        bot_thread.start()
     app.run(host=HOST, port=PORT)
